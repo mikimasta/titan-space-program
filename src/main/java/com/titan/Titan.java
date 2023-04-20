93addb8d7f4233008d1d58f9a47b1238cf73cc32
mikimasta <mikolaj.gawrys@gmail.com>
2023-04-20 12:43:12 +0200

    Initial commit

---
+++ src/main/java/com/titan/Titan.java
@@ -0,0 +1,7 @@
+package com.titan;
+
+public class Titan {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
---
+++ src/test/java/com/titan/TitanTest.java
@@ -0,0 +1,11 @@
+package com.titan;
+
+import org.junit.Test;
+
+public class TitanTest {
+
+	@Test
+	public void verifyNoExceptionThrown() {
+		Titan.main(new String[]{});
+	}
+}
