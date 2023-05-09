a8056d7ad7aab327399a4a605e8c89482b1ac95f
Emre Pelzer <72704986+emrepel03@users.noreply.github.com>
2023-05-09 12:02:00 +0200

    Create TwoStageAdamBashforth.java
    adam bashforth

---
+++ src/main/java/com/titan/math/TwoStageAdamBashforth.java
@@ -0,0 +1,30 @@
+package com.titan.math;
+
+/**
+ * Two-stage Adam Bashforth implements Solver: <br>
+ * implementation of the Euler-Solver approximation method <br>
+ * wi+1 = wi + h * f(wi, ti)
+ */
+
+public class TwoStageAdamBashforth implements Solver {
+
+    /**
+     * determines the step size for the Adam Bashforth solver
+     */
+    private final double stepSize;
+
+    /**
+     * this method calculates the positions and velocities of all celestial objects after every iteration using the Adam Bashforth method
+     * @param positions vector of all the positions
+     * @param velocities vector of all the velocities
+     * @param t step we are currently on
+     */
+
+    public Vector[] solve(Function f, Vector positions, Vector velocities, Vector masses, int t) {
+
+        //solver
+
+        return result;
+    }
+
+}
