6e67859dbda02eb2330d133289a87c083e92e3b0
mikimasta <mikolaj.gawrys@gmail.com>
2023-06-24 18:29:37 +0200

    full rotation now available

--- src/main/java/com/titan/controls/Hurricane.java
+++ src/main/java/com/titan/controls/Hurricane.java
@@ -11,7 +11,7 @@ public class Hurricane extends Wind { //120km/h - 170km/h
     Vector calculateVelocity(LandingModule module) {
         if (module.getY() > 0.05) {
 
-            currentWindSpeed = new Vector(new double[]{120d/3600d, 10d/3600d, 0});
+            currentWindSpeed = new Vector(new double[]{-120d/3600d, -40d/3600d, 0});
             return currentWindSpeed;
         }
         return new Vector(new double[]{0, 0, 0});
@@ -23,11 +23,12 @@ public class Hurricane extends Wind { //120km/h - 170km/h
     }
 
     @Override
-    public double getWindAngle() { 
-        
-        double sine = Math.sin(currentWindSpeed.getValue(1)/currentWindSpeed.getLength());
+    public double getWindAngle() {
 
-        return Math.toDegrees(sine);
+        double angle  = Math.asin(currentWindSpeed.getValue(1)/currentWindSpeed.getLength());
+
+
+        return currentWindSpeed.getValue(0) < 0 ? 180 - Math.toDegrees(angle) : Math.toDegrees(angle);//currentWindSpeed.getValue(0) < 0 ? -degrees : degrees;
 
     }
 }
